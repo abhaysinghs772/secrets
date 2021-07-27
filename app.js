33a3a6b53f5b3170b33b1357e6d6901a42d4be01
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-07-27 15:11:53 +0530

    user-name and password only (plain-text)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
 const ejs = require(`ejs`);
 
 const app = express();
@@ -9,18 +10,68 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get(`/`, (req, res)=>{
+// connects mongoose to mongoDB
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
+
+// create an userSchema on which new models are gonna be created
+const userSchema = {
+    email: String,
+    password: String
+}
+
+// creates an Users( plural ) Model on top of userSchema
+const User = new mongoose.model(`User`, userSchema);
+
+app.get(`/`, (req, res) => {
     res.render(`home`);
 });
 
-app.get(`/login`, (req, res)=>{
+app.get(`/login`, (req, res) => {
     res.render(`login`);
 });
 
-app.get(`/register`, (req, res)=>{
+app.get(`/register`, (req, res) => {
     res.render(`register`);
 });
 
+// register a new user to our web-app
+app.post(`/register`, (req, res) => {
+
+    // console.log(req.body.username, req.body.password);
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if (!err) {
+            res.render(`secrets`);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+// login
+app.post(`/login`, (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    // checks whether the given credentials exist or not
+    // if yes then log `user exist`
+    // else log `user does not exist`
+    User.findOne({email: userName, password: password}, (err, founduser)=>{
+        if(founduser){
+            // console.log(`user exist`, founduser);
+            res.render(`/secrets`);
+        }else{
+            // console.log(`user does not exist`, err);
+            
+        }
+    });
+
+})
 
 app.listen(3000, function () {
     console.log(`server is up`);
