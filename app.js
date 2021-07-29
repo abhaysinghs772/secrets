df73f91037f67257187e591fed5dbc912a7df9e7
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-07-29 23:55:20 +0530

    debugging - failed to serialize user

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
-const encrypt = require(`mongoose-encryption`);
+// const encrypt = require(`mongoose-encryption`);
 const ejs = require(`ejs`);
-const md5 = require(`md5`);
+// const md5 = require(`md5`);
+const session = require('express-session');
+const passport = require(`passport`);
+const passportLocalMongoose = require(`passport-local-mongoose`);
 
 const app = express();
 app.use(express.static(`public`));
@@ -14,8 +17,20 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// using session below
+app.use(session({
+    secret: `my little secret`,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connects mongoose to mongoDB
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
+mongoose.set(`useCreateIndex`, true);
 
 // creates an userSchema class on which new user objects are gonaa be created
 const userSchema = new mongoose.Schema({
@@ -23,12 +38,16 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// Secret String Instead of Two Keys
+userSchema.plugin(passportLocalMongoose);
 
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
-
-// creates an Users( plural ) Model on top of userSchema
 const User = new mongoose.model(`User`, userSchema);
+
+// Configure Passport/Passport-Local
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get(`/`, (req, res) => {
     res.render(`home`);
@@ -42,45 +61,50 @@ app.get(`/register`, (req, res) => {
     res.render(`register`);
 });
 
+/* checking if user is logged in or not
+ * if yes then renders to the `/secrets` route
+ * if not then redirects to the `/login` route
+ */
+app.get(`/secrets`, (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render(`/secrets`);
+    } else {
+        res.redirect(`/login`);
+    }
+});
+
 // register a new user to our web-app
 app.post(`/register`, (req, res) => {
-
-    // console.log(req.body.username, req.body.password);
-
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save((err) => {
-        if (!err) {
-            res.render(`secrets`);
-        } else {
+    User.register({ username: req.body.username}, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
+            res.redirect(`/register`);
+        } else {
+            // creates a logged in session and then redirects to secrets page
+            passport.authenticate(`local`)(req, res, function () {
+                res.redirect(`/secrets`);
+            });
         }
     });
 });
 
 // login
 app.post(`/login`, (req, res) => {
-    const userName = req.body.username;
-    const password = md5(req.body.password);
 
-    // checks whether the given credentials exist or not
-    // if yes then log `user exist`
-    // else log `user does not exist`
-    User.findOne({ email: userName}, (err, founduser) => {
-        if (founduser) {
-            if(founduser.password == password){
-                // console.log(`user exist`, founduser);
-                res.render(`secrets`);
-            }
-        } else {
-            // console.log(`user does not exist`, err);
-            console.log(err);
-        }
+    const user = new User ({
+        usernamme: req.body.username,
+        password: req.body.password
     });
 
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        } else {
+            passport.authenticate(`local`)(req, res, function () {
+                res.redirect(`/secrets`);
+            });
+        }
+    });
 })
 
 app.listen(3000, function () {
